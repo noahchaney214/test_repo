2b99c4c335204f6a085983b9a4b017696bcfde6e
Noah Chaney <117609324+noahchaney214@users.noreply.github.com>
2023-01-27 15:38:32 -0800

    Update main.py
    reformatted

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from person import Person
 
 def main():
-noah = Person("Noah Chaney", 21, 'm')
-noah.printPerson()
+	noah = Person("Noah Chaney", 21, 'm')
+	noah.printPerson()
 
 if __name__ == "__main__":
 	main()
